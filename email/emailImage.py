f3e886ad7d98e43fd11ef6c564146b734ce22e6d
Cassidy Skorczewski <cassidy.skor@gmail.com>
2018-11-12 14:14:29 -0700

    modifed email string

--- email/emailImage.py
+++ email/emailImage.py
@@ -3,6 +3,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import datetime
     
 def sendEmail(fromAddress, toAddress, password, contents, attachFile = None):
     """This function sends an email with the possibility to attach a file
@@ -40,13 +41,38 @@ def sendEmail(fromAddress, toAddress, password, contents, attachFile = None):
     text = msg.as_string()
     server.sendmail(fromAddress, toAddress, text)
     server.quit()
+
+def generateEmailBody(filename = None):
+    """This function generates the body of the email with the option
+        to include the filename of the image of unauthorized user.
+        
+        Inputs:
+            filename: name of image 
+        Outputs:
+            body: body of the email
+    """
     
+    body = "Hello,\n\nThere was an attempt to access your device at "
+    
+    now = datetime.datetime.now()
+    body += now.strftime("%Y-%m-%d %H:%M") + ".\n"
+    
+    if filename:
+        body += "The individual trying to access your account can be seen in the attached file "
+        body += filename + "."
+        body += " This individual was denied access to your account.\n"
+        
+    body += "\nYour friends at C & C Security."
+    return body
+
 if __name__ == "__main__":
     fromAddress = "CandCSec370@gmail.com"
     toAddress = "CandCSec370@gmail.com"
-    password = "grounds4lyfe"
-    attachFile = ["hacker.jpg", "./hacker.jpg"]
-    contents = ["Testing email alert", "This email is a test for the alerts\n\n Thanks."]
-    sendEmail(fromAddress, toAddress, password, contents)
+    password = #PUT GMAIL PASSWORD HERE
+    
+    attachFile = ["""IMAGE FILE NAME HERE, IMAGE PATHWAY HERE"""]
+    contents = ["Attempted Access", generateEmailBody(attachFile[0])]
+    
+    sendEmail(fromAddress, toAddress, password, contents, attachFile)
     print("Your email to %s has been sent." % toAddress)
     
