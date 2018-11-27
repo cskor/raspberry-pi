6ae67a1b55e3d06732cc5b41d9c03ac81a9a97c1
Cassidy <cassidy.skor@gmail.com>
2018-11-26 21:25:43 -0700

    testing

--- takeImage.py
+++ takeImage.py
@@ -1,10 +1,26 @@
 from picamera import PiCamera
 from time import sleep
+import emailImage
 
-if __name__ == "__main__":
+def takePhoto():
     camera = PiCamera()
     
     camera.start_preview()
-    sleep(5)
-    camera.capture('./image.jpg')
+    sleep(10)
+    camera.capture('./intruder.jpg')
     camera.stop_preview()
+
+def sendPhoto():
+    fromAddress = "CandCSec370@gmail.com"
+    toAddress = "cassidy.skor@gmail.com"
+    password = "grounds4lyfe"
+    
+    attachFile = ["intruder.jpg", "./intruder.jpg"]
+    contents = ["Attempted Access", emailImage.generateEmailBody(attachFile[0])]
+    
+    emailImage.sendEmail(fromAddress, toAddress, password, contents, attachFile)
+    print("Your email to %s has been sent." % toAddress)
+    
+if __name__ == "__main__":
+    takePhoto()
+    sendPhoto()
